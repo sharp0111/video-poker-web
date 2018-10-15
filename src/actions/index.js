1244731dee52f901777bd562318f8bed985a5e75
Res Yeg <shaqk702@gmail.com>
2018-10-15 12:55:24 -0700

    update actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,22 @@
 export const START_GAME = 'START_GAME';
 export const TOGGLE_CARD_STATE = 'TOGGLE_CARD_STATE';
+export const DRAW_CARD = 'DRAW_CARD';
 export const CALCULATE_CREDIT = 'CALCULATE_CREDIT';
+
+export const startGame = () => ({
+	type: START_GAME,
+});
+
+export const toggleCardState = (cardIdx) => ({
+	type: TOGGLE_CARD_STATE,
+	cardIdx
+});
+
+export const drawCard = (cardIdx) => ({
+	type: DRAW_CARD,
+	cardIdx
+});
+
+export const calculateCredit =  () => ({
+	type: CALCULATE_CREDIT,
+});
